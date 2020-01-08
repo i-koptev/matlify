6bbb497947c6d5ac92565b4ed7660e81ebf34c37
Igor Koptev <igor.koptef@gmail.com>
2020-01-08 12:15:33 +0200

    Added gatsby-source-filesystem plugin-config-dirs

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,31 @@
 module.exports = {
     /* Your site config here */
     plugins: [
+        "gatsby-plugin-react-helmet",
         "gatsby-plugin-top-layout",
+        {
+            // keep as first gatsby-source-filesystem plugin for gatsby image support
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/static/img`,
+                name: "uploads",
+            },
+        },
+
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: "pages",
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/img`,
+                name: "images",
+            },
+        },
         {
             resolve: "gatsby-plugin-material-ui",
             // If you want to use styled components you should change the injection order.
@@ -19,6 +43,5 @@ module.exports = {
         },
         // If you want to use styled components you should add the plugin here.
         // 'gatsby-plugin-styled-components',
-        "gatsby-plugin-react-helmet",
     ],
 }

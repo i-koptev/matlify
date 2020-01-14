76d133b1b3955b012627263b21533bef541419bb
Igor Koptev <igor.koptef@gmail.com>
2020-01-14 12:37:03 +0200

    Image removed from the About page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,39 +1,23 @@
-/* import React from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { Link, graphql } from "gatsby"
 
-export const AboutPageTemplate = ({ image, title, heading, subheading }) => (
+export const AboutPageTemplate = ({ title, html }) => (
     <div
         style={{
             maxWidth: "800px",
             margin: "0 auto",
         }}
     >
-        <div
-            style={{
-                height: "50vh",
-                backgroundImage: `url(${
-                    !!image.childImageSharp
-                        ? image.childImageSharp.fluid.src
-                        : image
-                })`,
-                backgroundPosition: `top left`,
-                backgroundAttachment: `fixed`,
-            }}
-        ></div>
         <div>
             <h1>{title}</h1>
-            <h3>{heading}</h3>
-            <h3>{subheading}</h3>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
         </div>
     </div>
 )
 
 AboutPageTemplate.propTypes = {
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     title: PropTypes.string,
-    heading: PropTypes.string,
-    subheading: PropTypes.string,
 }
 
 const AboutPage = ({ data }) => {
@@ -41,10 +25,8 @@ const AboutPage = ({ data }) => {
 
     return (
         <AboutPageTemplate
-            image={frontmatter.image}
             title={frontmatter.title}
-            heading={frontmatter.heading}
-            subheading={frontmatter.subheading}
+            html={data.markdownRemark.html}
         />
     )
 }
@@ -64,17 +46,8 @@ export const pageQuery = graphql`
         markdownRemark(frontmatter: { templateKey: { eq: "about-page" } }) {
             frontmatter {
                 title
-                image {
-                    childImageSharp {
-                        fluid(maxWidth: 1200, quality: 100) {
-                            ...GatsbyImageSharpFluid
-                        }
-                    }
-                }
-                heading
-                subheading
             }
+            html
         }
     }
 `
- */

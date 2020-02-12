4f9ec755958eaeab31cc3fcd6cccc562f5ecb6a2
Igor Koptev <igor.koptef@gmail.com>
2020-02-12 14:03:51 +0200

    blog ...

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,105 +1,87 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Link, graphql, StaticQuery } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import {
+    changeLocale,
+    injectIntl,
+    // Link,
+    FormattedMessage,
+} from "gatsby-plugin-intl"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import Container from "@material-ui/core/Container"
+import Grid from "@material-ui/core/Grid"
+
 import PreviewCompatibleImage from "./PreviewCompatibleImage"
 
-class BlogRoll extends React.Component {
-    render() {
-        const { data } = this.props
-        const { edges: posts } = data.allMarkdownRemark
+const useStyles = makeStyles(theme => ({
+    article: {
+        // outline: "3px solid tomato",
+        padding: "2.5rem 2rem",
+        backgroundColor: "rgba(255,255,255,0.9)",
+        marginBottom: "1rem",
+        "& img": {
+            border: "10px solid rgba(0,0,0,0.1)",
+        },
+    },
+    articleHeader: {
+        fontFamily: "PT Sans, sans-serif",
+        fontWeight: "700",
+        color: "tomato",
+        textTransform: "uppercase",
+    },
+    articleHeaderLink: {
+        textDecoration: "none !important",
+    },
+    articleMeta: {
+        fontFamily: "PT Sans, sans-serif",
+        fontWeight: "400",
+        color: "rgba(0,0,0,0.9)",
+        textTransform: "uppercase",
+        fontSize: "0.8rem",
+        letterSpacing: "0.05em",
+        padding: "0.6em 0 1.2em 0",
+        "& span": {
+            marginRight: "2em",
+        },
+    },
+    test: {
+        flexGrow: 1,
+        [theme.breakpoints.down("sm")]: {
+            backgroundColor: theme.palette.secondary.main,
+        },
+        [theme.breakpoints.up("md")]: {
+            backgroundColor: theme.palette.primary.main,
+        },
+        [theme.breakpoints.up("lg")]: {
+            backgroundColor: theme.palette.primary.ikky,
+        },
+    },
+}))
 
-        return (
-            <div className="columns is-multiline" style={{ color: "white" }}>
-                {posts &&
-                    posts.map(({ node: post }) => (
-                        <div className="is-parent column is-6" key={post.id}>
-                            <article
-                                className={`blog-list-item tile is-child box notification ${
-                                    post.frontmatter.featuredpost
-                                        ? "is-featured"
-                                        : ""
-                                }`}
-                            >
-                                <header>
-                                    {post.frontmatter.featuredimage ? (
-                                        <div className="featured-thumbnail">
-                                            <PreviewCompatibleImage
-                                                imageInfo={{
-                                                    image:
-                                                        post.frontmatter
-                                                            .featuredimage,
-                                                    alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                                }}
-                                            />
-                                        </div>
-                                    ) : null}
-                                    <p className="post-meta">
-                                        <Link
-                                            className="title has-text-primary is-size-4"
-                                            to={post.fields.slug}
-                                        >
-                                            {post.frontmatter.title}
-                                        </Link>
-                                        <span> &bull; </span>
-                                        <span className="subtitle is-size-5 is-block">
-                                            {post.frontmatter.date}
-                                        </span>
-                                    </p>
-                                </header>
-                                <p>
-                                    {post.excerpt}
-                                    <br />
-                                    <br />
-                                    <Link
-                                        className="button"
-                                        to={post.fields.slug}
-                                    >
-                                        Keep Reading →
-                                    </Link>
-                                </p>
-                            </article>
-                        </div>
-                    ))}
-            </div>
-        )
-    }
-}
-
-BlogRoll.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            edges: PropTypes.array,
-        }),
-    }),
-}
-
-export default () => (
-    <StaticQuery
-        query={graphql`
-            query BlogRollQuery {
-                allMarkdownRemark(
-                    sort: { order: DESC, fields: [frontmatter___date] }
-                    filter: {
-                        frontmatter: { templateKey: { eq: "blog-post" } }
-                    }
-                ) {
-                    edges {
-                        node {
-                            excerpt(pruneLength: 400)
-                            id
-                            fields {
-                                slug
-                            }
-                            frontmatter {
-                                title
-                                templateKey
-                                date(formatString: "MMMM DD, YYYY")
-                                featuredpost
-                                featuredimage {
-                                    childImageSharp {
-                                        fluid(maxWidth: 1280, quality: 100) {
-                                            ...GatsbyImageSharpFluid
-                                        }
+const BlogRoll = ({ intl }) => {
+    const qdata = useStaticQuery(graphql`
+        query BlogRollQuery {
+            allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date] }
+                filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+            ) {
+                edges {
+                    node {
+                        excerpt(pruneLength: 400)
+                        id
+                        fields {
+                            slug
+                        }
+                        frontmatter {
+                            title
+                            templateKey
+                            date(formatString: "DD. MM. YYYY")
+                            featuredpost
+                            featuredimage {
+                                childImageSharp {
+                                    fluid(maxWidth: 800, quality: 100) {
+                                        ...GatsbyImageSharpFluid
                                     }
                                 }
                             }
@@ -107,7 +89,92 @@ export default () => (
                     }
                 }
             }
-        `}
-        render={(data, count) => <BlogRoll data={data} count={count} />}
-    />
-)
+        }
+    `)
+    const { edges: posts } = qdata.allMarkdownRemark
+
+    const classes = useStyles()
+    const theme = useTheme()
+
+    return (
+        <div>
+            {posts &&
+                posts.map(({ node: post }) => (
+                    <div key={post.id}>
+                        <article
+                            className={`${classes.article}  ${
+                                post.frontmatter.featuredpost
+                                    ? "is-featured"
+                                    : ""
+                            }`}
+                        >
+                            <Link
+                                className={classes.articleHeaderLink}
+                                to={post.fields.slug}
+                            >
+                                <Typography
+                                    className={classes.articleHeader}
+                                    variant="h5"
+                                    component="h2"
+                                    // gutterBottom
+                                >
+                                    {intl.formatMessage({
+                                        id: `${post.id}.postTitle`,
+                                    })}
+                                </Typography>
+                            </Link>
+                            <Typography
+                                component="section"
+                                className={classes.articleMeta}
+                                variant="subtitle2"
+                                gutterBottom
+                            >
+                                <span>Posted: {post.frontmatter.date}</span>
+                                <span>Author: Admin</span>
+                                <span>Category: Programming</span>
+                            </Typography>
+
+                            {post.frontmatter.featuredimage ? (
+                                <PreviewCompatibleImage
+                                    imageInfo={{
+                                        image: post.frontmatter.featuredimage,
+                                        alt:
+                                            `featured image thumbnail for post` +
+                                            intl.formatMessage({
+                                                id: `${post.id}.postTitle`,
+                                            }),
+                                    }}
+                                />
+                            ) : null}
+                            <p>
+                                <div>
+                                    Excerpt goes here ... TODO in BlogRoll.js{" "}
+                                    <br />
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit. Quisquam possimus sint ut
+                                    a sapiente nulla quaerat dolorum earum et
+                                    aperiam! Eos molestias ab eaque. Magnam
+                                    culpa voluptatum adipisci magni itaque.
+                                </div>
+                                <br />
+                                <br />
+                                <Link className="" to={post.fields.slug}>
+                                    Keep Reading →
+                                </Link>
+                            </p>
+                        </article>
+                    </div>
+                ))}
+        </div>
+    )
+}
+/* 
+BlogRoll.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array,
+        }),
+    }),
+}
+ */
+export default injectIntl(BlogRoll)
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,38 +1,88 @@
 import React from "react"
 
+import {
+    changeLocale,
+    injectIntl,
+    Link,
+    FormattedMessage,
+} from "gatsby-plugin-intl"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
+import Grid from "@material-ui/core/Grid"
+
 import Layout from "../../components/Layout"
 import BlogRoll from "../../components/BlogRoll"
 
-export default class BlogIndexPage extends React.Component {
-    render() {
-        return (
-            <Layout>
-                <div
-                    className="full-width-image-container margin-top-0"
+const useStyles = makeStyles(theme => ({}))
+
+const BlogIndexPage = ({ intl }) => {
+    const classes = useStyles()
+    const theme = useTheme()
+
+    return (
+        <Layout>
+            <Container
+                maxWidth={theme.siteContainer.maxWidth}
+                // component="section"
+                // className="intro"
+                // className={classes.section}
+            >
+                <h1
                     style={{
-                        backgroundImage: `url('/img/blog-index.jpg')`,
+                        // backgroundColor: "rgba(232,232,232,0.05)",
+                        color: "rgba(232,232,232,0.8)",
+                        padding: "1rem 0",
+                        marginTop: "0",
                     }}
                 >
-                    <h1
-                        className="has-text-weight-bold is-size-1"
+                    {intl.formatMessage({
+                        id: `blogRollHeader`,
+                    })}
+                </h1>
+            </Container>
+
+            <Container
+                maxWidth={theme.siteContainer.maxWidth}
+                // disableGutters
+                // component="section"
+                // className={classes.section}
+            >
+                <Grid container spacing={8}>
+                    <Grid component="section" item xs={12} md={8}>
+                        <BlogRoll />
+                    </Grid>
+                    <Grid
+                        component="section"
+                        item
+                        xs={12}
+                        md={4}
                         style={{
-                            boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-                            backgroundColor: "#f40",
                             color: "white",
-                            padding: "1rem",
                         }}
                     >
-                        Latest Stories
-                    </h1>
-                </div>
-                <section className="section">
-                    <div className="container">
-                        <div className="content">
-                            <BlogRoll />
-                        </div>
-                    </div>
-                </section>
-            </Layout>
-        )
-    }
+                        <h3>Latest Posts /TODO/</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur. <br />
+                            Lorem ipsum dolor sit amet consectetur. <br />
+                            Lorem ipsum dolor sit amet consectetur. <br />
+                            Lorem ipsum dolor sit amet consectetur. <br />
+                            Lorem ipsum dolor sit amet consectetur. <br />
+                            Lorem ipsum dolor sit amet consectetur. <br />
+                        </p>
+                        <h3>Categories /TODO/</h3>
+                        <ol>
+                            <li>Item number #1</li>
+                            <li>Item number #1</li>
+                            <li>Item number #1</li>
+                            <li>Item number #1</li>
+                            <li>Item number #1</li>
+                            <li>Item number #1</li>
+                            <li>Item number #1</li>
+                        </ol>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Layout>
+    )
 }
+export default injectIntl(BlogIndexPage)

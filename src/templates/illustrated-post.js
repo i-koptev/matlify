49ef599dec93be233a706f1ce38a79f0e68b0877
Igor Koptev <igor.koptef@gmail.com>
2020-03-27 12:50:14 +0200

    tuning illustrated post to be like simple blog post

--- src/templates/illustrated-post.js
+++ src/templates/illustrated-post.js
@@ -1,14 +1,59 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
-import { injectIntl } from "gatsby-plugin-intl"
-import Layout from "../components/Layout"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
 
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+import { injectIntl } from "gatsby-plugin-intl"
+
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
 import Container from "@material-ui/core/Container"
 
-import Img from "gatsby-image"
+import { kebabCase } from "lodash"
+import Content, { HTMLContent } from "../components/Content"
 
-export const Vost = ({ intl, data }) => {
+import Layout from "../components/Layout"
+
+export const IllustratedPostTemplate = ({
+    content,
+    contentComponent,
+    description,
+    tags,
+    title,
+    helmet,
+}) => {
+    const PostContent = contentComponent || Content
+
+    return (
+        <div style={{ color: "white" }}>
+            {helmet || ""}
+
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+                <div>
+                    <h4>Tags</h4>
+                    <ul>
+                        {tags.map(tag => (
+                            <li key={tag + `tag`}>
+                                <Link to={`/tags/${kebabCase(tag)}/`}>
+                                    {tag}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : null}
+        </div>
+    )
+}
+
+export const IllustratedPost = ({ intl, data }) => {
+    const { markdownRemark: post } = data
+
     const theme = useTheme()
 
     return (
@@ -19,6 +64,15 @@ export const Vost = ({ intl, data }) => {
                 // className="intro"
                 // className={classes.section}
             >
+                <IllustratedPostTemplate
+                    title={intl.formatMessage({
+                        id: `${post.id}.postTitle`,
+                    })}
+                    description={intl.formatMessage({
+                        id: `${post.id}.postDescription`,
+                    })}
+                    tags={post.frontmatter.tags}
+                />
                 <div style={{ color: "#eee" }}>
                     <div>
                         {data.markdownRemark.frontmatter.illustratedPostBody.map(
@@ -51,9 +105,9 @@ export const Vost = ({ intl, data }) => {
     )
 }
 
-export default injectIntl(Vost)
+export default injectIntl(IllustratedPost)
 
-export const illustratedBlogPageQuery = graphql`
+export const illustratedPostPageQuery = graphql`
     query illustratedPostByID($id: String!) {
         markdownRemark(id: { eq: $id }) {
             id

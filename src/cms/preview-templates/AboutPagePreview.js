c004d0a95004d4a0ce682a339a566351354f6960
Igor Koptev <igor.koptef@gmail.com>
2020-04-06 13:19:53 +0300

    about page and about prewiev testing

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,19 +1,57 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import showdown from "showdown"
+import theme from "../../theme"
+import React from "react"
+import PropTypes from "prop-types"
+import { AboutPageTemplate } from "../../templates/about-page"
+import Content, { HTMLContent } from "../../components/Content"
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
-)
+const styles = {
+    wrapperStyle: {
+        fontFamily: theme.typography.fontFamily,
+        backgroundColor: "#eee",
+        padding: "2rem 3rem",
+    },
+    headerStyle: {
+        textAlign: "center",
+        fontFamily: theme.typography.fontFamily,
+        color: theme.palette.primary.main,
+    },
+}
+
+const AboutPagePreview = ({ entry, widgetFor }) => {
+    const converter = new showdown.Converter({ noHeaderId: true })
+    const lang = entry.getIn(["data", "adminlang"])
+    return (
+        <div style={styles.wrapperStyle}>
+            <h1 style={styles.headerStyle}>
+                {entry.getIn(["data", "aboutHeading", lang])}
+            </h1>
+            <h2>{entry.getIn(["data", "aboutSubheading", lang])}</h2>
+            <HTMLContent
+                content={converter.makeHtml(
+                    entry.getIn(["data", "aboutText", lang])
+                )}
+            ></HTMLContent>
+        </div>
+        // <div>
+        //     <AboutPageTemplate
+        //         heading={entry.getIn(["data", "aboutHeading", lang])}
+        //         subheading={entry.getIn(["data", "aboutSubheading", lang])}
+        //         // text={widgetFor("aboutText")}
+        //         text={converter.makeHtml(
+        //             entry.getIn(["data", "aboutText", lang])
+        //         )}
+        //         // content={widgetFor('body')}
+        //     />
+        // </div>
+    )
+}
 
 AboutPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+    entry: PropTypes.shape({
+        getIn: PropTypes.func,
+    }),
+    widgetFor: PropTypes.func,
 }
 
 export default AboutPagePreview
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,6 +1,14 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { Link, graphql } from "gatsby"
+
+import { graphql } from "gatsby"
+import { injectIntl } from "gatsby-plugin-intl"
+
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
+import Typography from "@material-ui/core/Typography"
+import Layout from "../components/Layout"
+
+import Content, { HTMLContent } from "../components/Content"
 
 /* 
 <SEO
@@ -9,45 +17,79 @@ import { Link, graphql } from "gatsby"
         lang="ru"
       />
 */
+const useStyles = makeStyles(theme => ({
+    test: {
+        color: "red",
+        // letterSpacing: theme.spacing(0.5),
+    },
+    wrapper: {
+        // outline: "3px solid tomato",
+        // paddingTop: theme.spacing(5),
+        // paddingBottom: theme.spacing(5),
+        // paddingLeft: theme.spacing(3),
+        // paddingRight: theme.spacing(3),
+        color: theme.palette.primary.main,
+        outline: "3px solid lime",
+        // margin: "1rem",
+        "& img": {
+            border: "10px solid rgba(0,0,0,0.1)",
+        },
+    },
+    container: {
+        // backgroundColor: "rgba(255,200,200,0.1)",
+        outline: "2px solid tomato",
+    },
+    htmlContent: {
+        color: "tomato",
+        "& h2": {
+            color: "red",
+            textAlign: "center",
+        },
+    },
+}))
 
-export const AboutPageTemplate = ({ title, html }) => (
-    <div
-        style={{
-            maxWidth: "800px",
-            margin: "0 auto",
-        }}
-    >
-        <div>
-            <h1>{title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-        </div>
-    </div>
-)
-
-AboutPageTemplate.propTypes = {
-    title: PropTypes.string,
-}
-
-const AboutPage = ({ data }) => {
-    const { frontmatter } = data.markdownRemark
+const AboutPage = ({ intl, data }) => {
+    const theme = useTheme()
+    const classes = useStyles()
 
     return (
-        <AboutPageTemplate
-            title={frontmatter.title}
-            html={data.markdownRemark.html}
-        />
+        <Layout>
+            <Container
+                maxWidth={theme.siteContainer.maxWidth}
+                component="section"
+                className={classes.container}
+            >
+                <div className={classes.wrapper}>
+                    <Typography
+                        className={classes.test}
+                        variant="h3"
+                        component="h1"
+                        align="center"
+                        // color="error"
+                        // color="textPrimary"
+                        // color={theme.palette.primary.ikky}
+                    >
+                        {intl.formatMessage({ id: `aboutHeading` })}
+                    </Typography>
+                    <Typography variant="h4" component="h2" align="center">
+                        {intl.formatMessage({ id: `aboutSubheading` })}
+                    </Typography>
+                    <div
+                        className={classes.htmlContent}
+                        dangerouslySetInnerHTML={{
+                            __html: intl.formatMessage({ id: `aboutText` }),
+                        }}
+                    ></div>
+                    {/* <HTMLContent
+                        content={intl.formatMessage({ id: `aboutText` })}
+                    ></HTMLContent> */}
+                </div>
+            </Container>
+        </Layout>
     )
 }
 
-AboutPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object,
-        }),
-    }),
-}
-
-export default AboutPage
+export default injectIntl(AboutPage)
 
 export const pageQuery = graphql`
     query AboutPageTemplate {
@@ -55,7 +97,6 @@ export const pageQuery = graphql`
             frontmatter {
                 title
             }
-            html
         }
     }
 `

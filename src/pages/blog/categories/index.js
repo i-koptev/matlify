67c02d009b3b80a6d7b401b6695a2ad2245e7a28
Igor Koptev <igor.koptef@gmail.com>
2020-04-21 13:22:36 +0300

    categories page edit

--- src/pages/blog/categories/index.js
+++ src/pages/blog/categories/index.js
@@ -1,5 +1,58 @@
 import React from "react"
 
-const CategoriesPage = () => <div style={{ color: "tomato" }}>Categories</div>
+import { injectIntl } from "gatsby-plugin-intl"
 
-export default CategoriesPage
+import Container from "@material-ui/core/Container"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+
+import Layout from "../../../components/Layout"
+
+const useStyles = makeStyles(theme => ({
+    test: {
+        color: "tomato",
+    },
+}))
+
+const CategoriesPage = ({
+    intl,
+    data: {
+        allMarkdownRemark: { group },
+    },
+}) => {
+    const theme = useTheme()
+    const classes = useStyles()
+    return (
+        <Layout>
+            <Container
+                maxWidth={theme.siteContainer.maxWidth}
+                // component="section"
+                // className="intro"
+                // className={classes.section}
+            >
+                <div className={classes.test}>All categories</div>
+                {group.map(category => (
+                    <div className={classes.test} key={category.fieldValue}>
+                        {intl.formatMessage({
+                            id: `${category.fieldValue}`,
+                        })}{" "}
+                        - {category.totalCount} статья
+                    </div>
+                ))}
+            </Container>
+        </Layout>
+    )
+}
+export default injectIntl(CategoriesPage)
+
+export const allCategoriesPageQuery = graphql`
+    query AllCategoriesQuery {
+        allMarkdownRemark(
+            filter: { frontmatter: { templateKey: { eq: "category-page" } } }
+        ) {
+            group(field: frontmatter___categoryId) {
+                totalCount
+                fieldValue
+            }
+        }
+    }
+`

e4c752e9cf55efb93e9fe3a5a467763a17666ab3
Igor Koptev <igor.koptef@gmail.com>
2020-04-28 11:17:10 +0300

    About page dev

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -11,7 +11,14 @@ import Layout from "../components/Layout"
 import { PrismCode } from "../components/Prism"
 
 import Content, { HTMLContent } from "../components/Content"
-
+import { Grid, withStyles, Button } from "@material-ui/core"
+import TestCard from "../components/TestCard"
+const styles = {
+    root: {
+        maxWidth: 345,
+        color: "white",
+    },
+}
 /* 
 <SEO
         title={title ? title - data.site.siteMetadata.title : data.site.siteMetadata.title}
@@ -21,38 +28,24 @@ import Content, { HTMLContent } from "../components/Content"
 */
 const useStyles = makeStyles(theme => ({
     container: {
-        outline: "2px solid tomato",
+        outline: theme.mainNavigationBorderBottom,
+        marginTop: `${theme.siteSpacing.aboutPage * 8}px`,
+        marginBottom: `${theme.siteSpacing.aboutPage * 8}px`,
         // backgroundColor: "rgba(255,200,200,0.1)",
-    },
-    wrapper: {
-        outline: "3px solid lime",
-        // backgroundColor: "rgba(255,200,200,0.1)",
-
-        paddingTop: theme.spacing(5),
-        // paddingBottom: theme.spacing(5),
-        // paddingLeft: theme.spacing(3),
-        // paddingRight: theme.spacing(3),
-        // color: theme.palette.primary.main,
-        // margin: "1rem",
-        "& img": {
-            border: "10px solid rgba(0,0,0,0.1)",
-        },
-    },
-    header: {
-        // color: "red",
-        // letterSpacing: theme.spacing(0.5),
     },
     htmlContent: {
-        color: theme.typography.body1.color,
         "& h2": {
-            color: "teal",
-            // color: theme.typography.h2.color,
-            fontFamily: theme.typography.h2.fontFamily,
-            fontWeight: theme.typography.h2.fontWeight,
-            // color: "red",
-            textAlign: "center",
+            ...theme.typography.h5,
+            paddingBottom: `${theme.siteSpacing.aboutPage * 4}px`,
+            paddingLeft: `${theme.siteSpacing.aboutPage * 8}px`,
+            // textAlign: "center",
         },
+        "& p": {
+            ...theme.html.paragraph,
+            // color: "olive",
+        }, //переопределение!!!
     },
+    paragraph: theme.html.paragraph,
 }))
 
 const code = `
@@ -76,50 +69,149 @@ const AboutPage = ({ intl, data }) => {
                 component="section"
                 className={classes.container}
             >
-                <PrismCode
+                <TestCard />
+                {/* <PrismCode
                     code={code}
                     language="jsx"
                     plugins={["line-numbers"]}
-                />
-                <div className={classes.wrapper}>
-                    <Typography
-                        className={classes.header}
-                        variant="h3"
-                        component="h1"
-                        align="center"
-                        // color="error"
-                        // color="textPrimary"
-                        // color={theme.palette.primary.ikky}
-                    >
-                        {intl.formatMessage({ id: `aboutHeading` })}
-                    </Typography>
-                    <Typography variant="h4" component="h2" align="center">
-                        {intl.formatMessage({ id: `aboutSubheading` })}
-                    </Typography>
-                    <div
-                        className={classes.htmlContent}
-                        dangerouslySetInnerHTML={{
-                            __html: intl.formatMessage({ id: `aboutText` }),
-                        }}
-                    ></div>
-                    <Typography variant="body1" gutterBottom>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Iste officiis assumenda excepturi dignissimos!
-                        Neque, nostrum expedita, tempora suscipit dolorum
-                        quaerat cum rerum quod reiciendis cumque at voluptate!
-                        Reiciendis, tenetur veniam?
-                    </Typography>
-                    <Typography variant="body2" gutterBottom>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Iste officiis assumenda excepturi dignissimos!
-                        Neque, nostrum expedita, tempora suscipit dolorum
-                        quaerat cum rerum quod reiciendis cumque at voluptate!
-                        Reiciendis, tenetur veniam?
-                    </Typography>
-                    {/* <HTMLContent
+                /> */}
+                <Grid
+                    container
+                    direction="column"
+                    spacing={theme.siteSpacing.aboutPage}
+                >
+                    <Grid item>
+                        <Typography
+                            variant="h3"
+                            component="h1"
+                            align="center"
+                            // color="error"
+                            // color="textPrimary"
+                            // color={theme.palette.primary.ikky}
+                        >
+                            {intl.formatMessage({ id: `aboutHeading` })}
+                        </Typography>
+                        <Typography variant="h4" component="h2" align="center">
+                            {intl.formatMessage({ id: `aboutSubheading` })}
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <div
+                            className={classes.htmlContent}
+                            dangerouslySetInnerHTML={{
+                                __html: intl.formatMessage({
+                                    id: `aboutText`,
+                                }),
+                            }}
+                        ></div>
+                    </Grid>
+                    <Grid item>
+                        <Grid container spacing={theme.siteSpacing.aboutPage}>
+                            <Grid
+                                item
+                                xs={12}
+                                md={4}
+                                component="section"
+                                id="footerSection"
+                                className="footerSection"
+                            >
+                                <Typography
+                                    variant="h5"
+                                    align="center"
+                                    gutterBottom
+                                >
+                                    Section 1
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    paragraph
+                                    className={classes.paragraph}
+                                >
+                                    Lorem ipsum dolor siiit, amet consectetur
+                                    adipisicing elit. Iste officiis assumenda
+                                    excepturi dignissimos! Neque, nostrum
+                                    expedita, tempora suscipit dolorum quaerat
+                                    cum rerum quod reiciendis cumque at
+                                    voluptate! Reiciendis, tenetur veniam?
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    className={classes.paragraph}
+                                >
+                                    Lorem ipsum dolor sit, amet consectetur
+                                    adipisicing elit. Iste officiis assumenda
+                                    excepturi dignissimos! Neque, nostrum
+                                    expedita, tempora suscipit dolorum quaerat
+                                    cum rerum quod reiciendis cumque at
+                                    voluptate! Reiciendis, tenetur veniam?
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} md={4}>
+                                <Typography
+                                    variant="h5"
+                                    align="center"
+                                    gutterBottom
+                                >
+                                    Section 2
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    paragraph="true"
+                                    className={classes.paragraph}
+                                >
+                                    Lorem ipsum dolor sit, amet consectetur
+                                    adipisicing elit. Iste officiis assumenda
+                                    excepturi dignissimos! Neque, nostrum
+                                    expedita, tempora suscipit dolorum quaerat
+                                    cum rerum quod reiciendis cumque at
+                                    voluptate! Reiciendis, tenetur veniam?
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    className={classes.paragraph}
+                                >
+                                    Lorem ipsum d dolor sit, amet consectetur
+                                    adipisicing elit. Iste officiis assumenda
+                                    excepturi dignissimos! Neque, nostrum
+                                    expedita, tempora suscipit dolorum quaerat
+                                    cum rerum quod reiciendis cumque at
+                                    voluptate! Reiciendis, tenetur veniam?
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} md={4}>
+                                <Typography
+                                    variant="h5"
+                                    align="center"
+                                    gutterBottom
+                                >
+                                    Section 3
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    gutterBottom
+                                    className={classes.paragraph}
+                                >
+                                    Lorem ipsum dolor sit, amet consectetur
+                                    adipisicing elit. Iste officiis assumenda
+                                    excepturi dignissimos! Neque, nostrum
+                                    expedita, tempora suscipit dolorum quaerat
+                                    cum rerum quod reiciendis cumque at
+                                    voluptate! Reiciendis, tenetur veniam?
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                {/* <HTMLContent
                         content={intl.formatMessage({ id: `aboutText` })}
                     ></HTMLContent> */}
-                </div>
+                {/* <pre style={{ color: "white" }}>
+                    {JSON.stringify(theme, null, 4)}
+                </pre> */}
             </Container>
         </Layout>
     )

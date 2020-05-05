7a1ad4bb7b9efad7c3f0ba207fbb75feb7d3d7f7
Igor Koptev <igor.koptef@gmail.com>
2020-05-05 13:39:18 +0300

    SvgCompatibleImage created (clone of PrCompImage)

---
+++ src/components/SvgCompatibleImage.js
@@ -0,0 +1,42 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Img from "gatsby-image"
+
+const SvgCompatibleImage = ({ imageInfo }) => {
+    const imageStyle = {}
+    //   const imageStyle = { borderRadius: '5px' }
+    const { alt = "", childImageSharp, image } = imageInfo
+
+    if (!!image && !!image.childImageSharp) {
+        return (
+            <Img
+                style={imageStyle}
+                fluid={image.childImageSharp.fluid}
+                alt={alt}
+            />
+        )
+    }
+
+    if (!!childImageSharp) {
+        return (
+            <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />
+        )
+    }
+
+    if (!!image && typeof image === "string")
+        return <img style={imageStyle} src={image} alt={alt} />
+
+    return null
+}
+
+SvgCompatibleImage.propTypes = {
+    imageInfo: PropTypes.shape({
+        alt: PropTypes.string,
+        childImageSharp: PropTypes.object,
+        image: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+            .isRequired,
+        style: PropTypes.object,
+    }).isRequired,
+}
+
+export default SvgCompatibleImage

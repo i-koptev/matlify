2b14846e61e1b22c060b7f17ee703b91410d0f94
Igor Koptev <igor.koptef@gmail.com>
2020-06-03 14:55:24 +0300

    experimenting with grid

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -26,8 +26,8 @@ const useStyles = makeStyles(theme => ({
         // padding: "1rem",
     },
     wrapper: {
-        marginTop: 0,
-        marginBottom: 0,
+        // marginTop: 0,
+        // marginBottom: 0,
     },
 }))
 
@@ -37,10 +37,329 @@ const GridPage = () => {
 
     const small = useMediaQuery(theme.breakpoints.down("sm"))
     const medium = useMediaQuery(theme.breakpoints.down("md"))
-    const firstOuter = small ? 1 : medium ? 3 : 5
-    const firstInner = small ? 1 : medium ? 3 : 5
+    const firstOuter = small ? 2 : medium ? 3 : 5
+    const firstInner = small ? 2 : medium ? 3 : 5
     return (
         <Layout>
+            {/* Блог */}
+            <div
+                style={{
+                    // margin: "1rem 0",
+                    backgroundColor: "maroon",
+                    height: "80px",
+                    color: "white",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontWeight: 400,
+                }}
+            >
+                <h2
+                    style={{
+                        fontWeight: 400,
+                        textTransform: "uppercase",
+                        letterSpacing: "0.2rem",
+                    }}
+                >
+                    Блог{" "}
+                </h2>
+            </div>
+            <Container
+                //  defined in pages/blog/index.js
+                maxWidth={theme.siteContainer.maxWidth}
+            >
+                <Grid
+                    container
+                    spacing={10}
+                    style={{
+                        color: "white",
+                        // backgroundColor: "tomato",
+                    }}
+                >
+                    {/* defined in pages/blog/index.js */}
+                    <Grid component="section" item xs={12} md={8}>
+                        {/* defined in BlogRoll */}
+                        <Grid container>
+                            <Grid
+                                item
+                                xs={12}
+                                md={5}
+                                style={{
+                                    backgroundColor: "tomato",
+                                }}
+                            >
+                                {" "}
+                                <div
+                                    style={{
+                                        minHeight: 383,
+                                        // backgroundColor: "navy",
+                                    }}
+                                ></div>
+                            </Grid>
+                            <Grid
+                                item
+                                xs={12}
+                                md={7}
+                                style={{
+                                    backgroundColor: "teal",
+                                }}
+                            >
+                                {" "}
+                                <Grid
+                                    container
+                                    // spacing={10}
+                                    direction="column"
+                                    justify="space-between"
+                                    wrap="nowrap"
+                                    // alignItems="flex-start"
+                                    style={{
+                                        minHeight: 383,
+                                        padding: 40,
+                                        paddingTop: 20,
+                                        paddingBottom: 20,
+                                        overflow: "hidden",
+                                    }}
+                                >
+                                    <Grid item>
+                                        <div
+                                            style={{
+                                                minHeight: 70,
+                                                backgroundColor: "tomato",
+                                            }}
+                                        >
+                                            sdfsdf
+                                        </div>
+                                    </Grid>
+                                    <Grid item>
+                                        <div
+                                            style={{
+                                                minHeight: 70,
+                                                backgroundColor: "tomato",
+                                            }}
+                                        >
+                                            sdfsdf
+                                        </div>
+                                    </Grid>
+                                    <Grid
+                                        item
+                                        container
+                                        justify="flex-end"
+                                        alignItems="flex-end"
+                                    >
+                                        <div
+                                            style={{
+                                                // minHeight: 70,
+                                                backgroundColor: "tomato",
+                                                padding: "1rem 2rem",
+                                                border: "2px solid black",
+                                            }}
+                                        >
+                                            BUTTON
+                                        </div>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        component="section"
+                        item
+                        xs={12}
+                        md={4}
+                        style={
+                            {
+                                // backgroundColor: "teal",
+                            }
+                        }
+                    >
+                        <div
+                            style={{
+                                minHeight: 200,
+                                backgroundColor: "navy",
+                            }}
+                        >
+                            LatestPosts AllCategoriesList
+                        </div>
+                    </Grid>
+                </Grid>
+            </Container>
+
+            {/* Блоки с фоном до границ области просмотра */}
+            <div
+                style={{
+                    // margin: "1rem 0",
+                    backgroundColor: "maroon",
+                    height: "80px",
+                    color: "white",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontWeight: 400,
+                }}
+            >
+                <h2
+                    style={{
+                        fontWeight: 400,
+                        textTransform: "uppercase",
+                        letterSpacing: "0.2rem",
+                    }}
+                >
+                    Блоки с фоном до границ области просмотра{" "}
+                </h2>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                }}
+            >
+                <div
+                    style={{
+                        backgroundColor: "navy",
+                        minHeight: "200px",
+                        width: "calc(1472px / 12 * 4 + (100% - 1472px) / 2)",
+                    }}
+                >
+                    <div
+                        style={{
+                            paddingLeft: "32px",
+                            minHeight: "200px",
+                            maxWidth: "calc( 1472px / 12 * 4 )",
+                            marginLeft: "auto",
+                        }}
+                    >
+                        <div
+                            style={{
+                                minHeight: "200px",
+                                backgroundColor: "#ccc3",
+                                padding: "1rem",
+                                color: "#eee",
+                            }}
+                        >
+                            <h3>Heading</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Incidunt aperiam consectetur
+                                voluptatibus debitis excepturi impedit, delectus
+                                sed iure nisi. Quos impedit obcaecati debitis
+                                cum magni unde veritatis aspernatur,
+                                perspiciatis corporis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style={{
+                        backgroundColor: "tomato",
+                        minHeight: "200px",
+                        width: "calc(1472px / 12 * 8 + (100% - 1472px) / 2)",
+                    }}
+                >
+                    {" "}
+                    <div
+                        style={{
+                            paddingRight: "32px",
+                            height: "200px",
+                            maxWidth: "calc( 1472px / 12 * 8 )",
+                            marginRight: "auto",
+                        }}
+                    >
+                        <div
+                            style={{
+                                height: "200px",
+                                backgroundColor: "#ccc3",
+                                padding: "1rem",
+                                color: "#eee",
+                            }}
+                        >
+                            <h3>Heading</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Corporis eligendi blanditiis
+                                architecto, quos adipisci, quas illo quo ipsam
+                                iste earum sunt vero, autem iusto odio nobis est
+                                illum reprehenderit nemo!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    // marginBottom: "1rem",
+                }}
+            >
+                <div
+                    style={{
+                        backgroundColor: "teal",
+                        minHeight: "200px",
+                        width: "calc(1472px / 12 * 8 + (100% - 1472px) / 2)",
+                    }}
+                >
+                    {" "}
+                    <div
+                        style={{
+                            paddingLeft: "32px",
+                            height: "200px",
+                            maxWidth: "calc( 1472px / 12 * 8 )",
+                            marginLeft: "auto",
+                        }}
+                    >
+                        <div
+                            style={{
+                                height: "200px",
+                                backgroundColor: "#ccc3",
+                                padding: "1rem",
+                                color: "#eee",
+                            }}
+                        >
+                            <h3>Heading</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Corporis eligendi blanditiis
+                                architecto, quos adipisci, quas illo quo ipsam
+                                iste earum sunt vero, autem iusto odio nobis est
+                                illum reprehenderit nemo!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style={{
+                        backgroundColor: "navy",
+                        minHeight: "200px",
+                        width: "calc(1472px / 12 * 4 + (100% - 1472px) / 2)",
+                    }}
+                >
+                    <div
+                        style={{
+                            paddingRight: "32px",
+                            minHeight: "200px",
+                            maxWidth: "calc( 1472px / 12 * 4 )",
+                            marginRight: "auto",
+                        }}
+                    >
+                        <div
+                            style={{
+                                minHeight: "200px",
+                                backgroundColor: "#ccc3",
+                                padding: "1rem",
+                                color: "#eee",
+                            }}
+                        >
+                            <h3>Heading</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Incidunt aperiam consectetur
+                                voluptatibus debitis excepturi impedit, delectus
+                                sed iure nisi. Quos impedit obcaecati debitis
+                                cum magni unde veritatis aspernatur,
+                                perspiciatis corporis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Container
                 maxWidth={theme.siteContainer.maxWidth}
                 // component="section"
